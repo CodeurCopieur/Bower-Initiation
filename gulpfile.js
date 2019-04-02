030ab6ab2ae8eb6b6dfb650ae0888d5fce6be729
codwerk <louis_widdy@live.fr>
2019-04-03 00:16:24 +0200

    add tack fonts(FontAwesome)

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ const paths = {
         css: {
             src : ['AppBundle/Ressources/bower_components/bootstrap/scss/bootstrap.scss', 'src/scss/index.scss'],
             dest : 'build/assets/css'
+        },
+       fonts: {
+            src : ['AppBundle/Ressources/bower_components/Font-Awesome/scss/*.scss'],
+            dest : 'build/assets/fonts'
         }
     }
 
@@ -14,16 +18,28 @@ const paths = {
 const clean = () => del([paths.css.dest]);
 
 function css() {
+     //où est mon fichier scss
     return gulp.src(paths.css.src)
+    //passer ce fichier par le compilateur sass
     .pipe(sass())
     .pipe(concat('index.css'))
+    //Où puis-je sauvegarder le scss compilé ?
     .pipe(gulp.dest(paths.css.dest))
+}
+
+function fonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(sass())
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest(paths.fonts.dest))
 }
 
 const build = gulp.series(clean, gulp.parallel(css));
 
 exports.clean = clean;
 exports.css = css;
+exports.fonts = fonts;
 exports.build = build;
 
+//Nous assignons également cette nouvelle tâche build comme étant notre tâche par défaut :
 gulp.task('default', build);
