9d4a3c6b65e4096fedb1c4690facde371279432a
codwerk <louis_widdy@live.fr>
2019-04-02 22:27:00 +0200

    add gulp-sass, gulp-concat

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+      sass = require('gulp-sass');
+      concat = require('gulp-concat');
+
+const paths = {
+        css: {
+            src : ['AppBundle/Ressources/bower_components/bootstrap/scss/bootstrap.scss', 'src/scss/index.scss'],
+            dest : 'build/assets/css'
+        }
+    }
+
+function css() {
+    return gulp.src(paths.css.src)
+    .pipe(sass())
+    .pipe(concat('index.css'))
+    .pipe(gulp.dest(paths.css.dest))
+}
+
+exports.css = css;
